ea86599487934f2fd76b3e6872123f7ddd13f987
Maria Cota <you@example.com>
2023-06-08 19:46:20 -0300

    soma

--- Base1/investindo.py
+++ Base1/investindo.py
@@ -3,3 +3,11 @@ print("Vina esteve aqui")
 print("maria esteve aqui")
 print("Olá, amor!")
 print("oi?")
+
+valor = 1000
+ano = 1
+Rendimento_ao_ano = 10/100
+
+soma = valor * Rendimento_ao_ano * ano
+
+print(soma)
